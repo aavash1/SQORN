2f1ed27b12444969eaac594e51bdc5166e889de4
Aziz Hasanov <aziz.hasanov.kh@gmail.com>
2018-11-23 00:09:49 +0900

    knn algorithm update, apache common collection lib added

--- RTOPKImplementationInRoadNetwork/src/algorithm/KnnAlgorithm.java
+++ RTOPKImplementationInRoadNetwork/src/algorithm/KnnAlgorithm.java
@@ -11,14 +11,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import framework.Graph;
 import framework.Path;
 
 public class KnnAlgorithm implements Algorithm {
 
 	private Graph graph;
-	
-	
 
 	public KnnAlgorithm(Graph graph) {
 		this.graph = graph;
@@ -33,9 +33,16 @@ public class KnnAlgorithm implements Algorithm {
 
 		int numberofNeighbor = resultSet.keySet().size();
 
-		ArrayList<Integer> keyInt = new ArrayList<Integer>(resultSet.keySet());
-		ArrayList<Double> valDoub = new ArrayList<Double>(resultSet.values());
-		HashMap<Integer, Double> pruned = new HashMap<Integer, Double>();
+//		ArrayList<Integer> keyInt = new ArrayList<Integer>(resultSet.keySet());
+//		ArrayList<Double> valDoub = new ArrayList<Double>(resultSet.values());
+
+		ArrayList<Integer> keyInt = new ArrayList<Integer>();
+		ArrayList<Double> valDoub = new ArrayList<Double>();
+		
+		CollectionUtils.addAll(keyInt, resultSet.keySet());
+		CollectionUtils.addAll(valDoub, resultSet.values());
+
+		HashMap<Integer, Double> pruned = new LinkedHashMap<Integer, Double>();
 
 		if (numberofK > numberofNeighbor) {
 			System.out.println("The requested number of K is more than the neighbors preseted");
@@ -84,17 +91,16 @@ public class KnnAlgorithm implements Algorithm {
 	}
 
 	public Map<Integer, Double> getKNNNodesWithDistance2(int queryPointNode, int numberofK) {
-		
+
 		Map<Integer, Double> finalReturnMap = new LinkedHashMap<Integer, Double>();
-		
+
 		List<Path> arrListOfPaths = new ArrayList<Path>();
-		
+
 		Map<Integer, Double> tempNeighborNodeMap = new LinkedHashMap<Integer, Double>();
-		
+
 		tempNeighborNodeMap = getNeighborNodesWithDistancesAscOrder(queryPointNode);
 
 		int numberofNeighbor = tempNeighborNodeMap.keySet().size();
-		
 
 		ArrayList<Integer> keyInt = new ArrayList<Integer>(tempNeighborNodeMap.keySet());
 		ArrayList<Double> valDoub = new ArrayList<Double>(tempNeighborNodeMap.values());
@@ -104,7 +110,6 @@ public class KnnAlgorithm implements Algorithm {
 		path.addNode(queryPointNode);
 		arrListOfPaths.add(path);
 
-		
 		if (numberofK > numberofNeighbor) {
 			System.out.println("The requested number of K is more than number of direct neighbors");
 			finalReturnMap = tempNeighborNodeMap;
@@ -112,62 +117,59 @@ public class KnnAlgorithm implements Algorithm {
 
 			ArrayList<Integer> arrListOfNewNodes = new ArrayList<>();
 			ArrayList<Map<Integer, Double>> arrListOfNewNodesWithDist = new ArrayList<Map<Integer, Double>>();
-						
-			//Map<Integer, Double> tempResultMap = new HashMap<>();
-			//tempResultMap = tempNeighborNodeMap;
-			while (intFoundNearestNeighbors < numberofK) { 
-				//tempNeighborNodeMap = getNeighborNodesWithDistancesAscOrder(queryPointNode);
 
-				Set<Integer> keySet = new HashSet<Integer>();			
+			// Map<Integer, Double> tempResultMap = new HashMap<>();
+			// tempResultMap = tempNeighborNodeMap;
+			while (intFoundNearestNeighbors < numberofK) {
+				// tempNeighborNodeMap = getNeighborNodesWithDistancesAscOrder(queryPointNode);
+
+				Set<Integer> keySet = new HashSet<Integer>();
 				keySet = tempNeighborNodeMap.keySet();
 				ArrayList<Integer> arrListTempNodes = new ArrayList<Integer>();
-				for (Integer key : keySet) { 
+				for (Integer key : keySet) {
 					arrListTempNodes = getNeighborNodesAscOrder(key);
-					
-					
-					
+
 					for (int i = 0; i < arrListTempNodes.size(); i++) {
-						if (arrListTempNodes.contains(arrListTempNodes.get(i))) { 
+						if (arrListTempNodes.contains(arrListTempNodes.get(i))) {
 							System.out.println(arrListTempNodes.get(i) + " in key set");
 							continue;
 						}
 						System.out.println(arrListTempNodes.get(i));
-					}					
-//					
+					}
+					//
 					System.out.println("not head key: " + key);
-					
+
 				}
-//				for (Integer i = 0; i < tempResultMap.size(); i++) {
-//					
-//					//finalReturnMap 
-//				}
-				
+				// for (Integer i = 0; i < tempResultMap.size(); i++) {
+				//
+				// //finalReturnMap
+				// }
+
 				for (Map.Entry<Integer, Double> entry : tempNeighborNodeMap.entrySet()) {
-				    //System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-				    finalReturnMap.put(entry.getKey(), entry.getValue());
+					// System.out.println("Key = " + entry.getKey() + ", Value = " +
+					// entry.getValue());
+					finalReturnMap.put(entry.getKey(), entry.getValue());
 				}
 				for (Map.Entry<Integer, Double> entry : finalReturnMap.entrySet()) {
-				    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+					System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
 				}
-				//System.out.println(intFoundNearestNeighbors);
+				// System.out.println(intFoundNearestNeighbors);
 				intFoundNearestNeighbors++;
-				
-				
-			}		
-			
-//			/// test start
-//			ArrayList<Integer> testResultList = new ArrayList<Integer>();
-//			testResultList = getNeighborNodes(3);
-//			System.out.println(testResultList);
-//
-//			testResultList = getNeighborNodesAscOrder(3);
-//			System.out.println(testResultList);
-//
-//			Map<Integer, Double> testResultSet = new HashMap<>();
-//			testResultSet = getNeighborNodesWithDistancesAscOrder(3);
-//			System.out.println(testResultSet);
-//			/// test end
-			
+
+			}
+
+			// /// test start
+			// ArrayList<Integer> testResultList = new ArrayList<Integer>();
+			// testResultList = getNeighborNodes(3);
+			// System.out.println(testResultList);
+			//
+			// testResultList = getNeighborNodesAscOrder(3);
+			// System.out.println(testResultList);
+			//
+			// Map<Integer, Double> testResultSet = new HashMap<>();
+			// testResultSet = getNeighborNodesWithDistancesAscOrder(3);
+			// System.out.println(testResultSet);
+			// /// test end
 
 		} else {
 			for (int i = numberofK; i < numberofNeighbor; i++) {
@@ -185,9 +187,178 @@ public class KnnAlgorithm implements Algorithm {
 
 	}
 
+	public Map<Integer, Double> getKNNNodesWithDistance3(int sourceNode, int numberOfK) {
+
+		Map<Integer, Double> mapDistinctNodesShortestDistToSrc = new LinkedHashMap<Integer, Double>();
+		Map<Integer, Double> mapTempDistinctNodesShortestDistToSrc = new LinkedHashMap<Integer, Double>();
+
+		Map<Integer, Double> mapFinalReturnNodes = new LinkedHashMap<Integer, Double>();
+		Map<Integer, Double> mapTempNodes = new LinkedHashMap<Integer, Double>();
+		Map<Integer, Double> mapTempChosenNodes = new LinkedHashMap<Integer, Double>();
+
+		int tempNumOfNeighbors = 0;
+		int numOfTotalFoundNodes = 0, numOfNewNodes, numOfTakeNodes;
+		int headNode = sourceNode;
+		int indexOfNewNode = 0;
+		//ArrayList<Integer> listUsedNewNodeIndex = new ArrayList<>();
+		
+		
+		ArrayList<Integer> newNodes = new ArrayList<Integer>();
+		
+		
+
+		ArrayList<Integer> keyInt = new ArrayList<Integer>();
+		ArrayList<Double> valDoub = new ArrayList<Double>();
+		//HashMap<Integer, Double> prunedMap = new LinkedHashMap<Integer, Double>();
+		
+		while (numOfTotalFoundNodes < numberOfK) {
+
+			mapTempNodes = getNeighborNodesWithDistancesAscOrder(headNode);
+			tempNumOfNeighbors = mapTempNodes.size();
+	
+			CollectionUtils.addAll(keyInt, mapTempNodes.keySet());
+			CollectionUtils.addAll(valDoub, mapTempNodes.values());
+			
+			
+			if (numberOfK > tempNumOfNeighbors) {
+
+				for (Map.Entry<Integer, Double> entry : mapTempNodes.entrySet()) {
+					// System.out.println("temp neighbor Key = " + entry.getKey() + ", temp neighbor
+					// Value = " + entry.getValue());
+					
+					if (!mapFinalReturnNodes.containsKey(entry.getKey())) { 
+						mapFinalReturnNodes.put(entry.getKey(), entry.getValue());
+						numOfTotalFoundNodes ++; 
+						newNodes.add(entry.getKey());
+						
+					}					
+					//indexOfNewNode = newNodes.get(0);
+					if (!mapTempDistinctNodesShortestDistToSrc.containsKey(entry.getKey())) {
+						mapTempDistinctNodesShortestDistToSrc.put(entry.getKey(), entry.getValue());
+					}
+					
+				}			
+				
+				
+
+				mapTempChosenNodes = mapTempNodes;
+
+			} else {
+
+				
+				for (int i = numberOfK; i < tempNumOfNeighbors; i++) {
+					keyInt.remove(numberOfK);
+					valDoub.remove(numberOfK);
+				}
+
+				for (int i = 0; i < numberOfK; i++) {
+					mapFinalReturnNodes.put(keyInt.get(i), valDoub.get(i));
+				}				
+				numOfTotalFoundNodes++;
+				
+				
+				
+			}
+			numOfTotalFoundNodes++;
+			headNode = 4;//indexOfNewNode; // assign dynamically
+			
+		}
+
+//		Map<Integer, Double> finalReturnMap = new LinkedHashMap<Integer, Double>();
+//
+//		List<Path> arrListOfPaths = new ArrayList<Path>();
+//
+//		Map<Integer, Double> tempNeighborNodeMap = new LinkedHashMap<Integer, Double>();
+//
+//		tempNeighborNodeMap = getNeighborNodesWithDistancesAscOrder(queryPointNode);
+//
+//		int numberofNeighbor = tempNeighborNodeMap.keySet().size();
+//		int intFoundNearestNeighbors = numberofNeighbor;
+//
+//		ArrayList<Integer> keyInt = new ArrayList<Integer>(tempNeighborNodeMap.keySet());
+//		ArrayList<Double> valDoub = new ArrayList<Double>(tempNeighborNodeMap.values());
+//		HashMap<Integer, Double> pruned = new HashMap<Integer, Double>();
+//
+//		Path path = new Path();
+//		path.addNode(queryPointNode);
+//		arrListOfPaths.add(path);
+//
+//		// System.out.println("The requested number of K is more than number of direct
+//		// neighbors");
+//		finalReturnMap = tempNeighborNodeMap;
+//
+//		ArrayList<Integer> arrListOfNewNodes = new ArrayList<>();
+//		ArrayList<Map<Integer, Double>> arrListOfNewNodesWithDist = new ArrayList<Map<Integer, Double>>();
+//
+//		// Map<Integer, Double> tempResultMap = new HashMap<>();
+//		// tempResultMap = tempNeighborNodeMap;
+//		while (intFoundNearestNeighbors < numberofK) {
+//			// tempNeighborNodeMap = getNeighborNodesWithDistancesAscOrder(queryPointNode);
+//
+//			Set<Integer> keySet = new HashSet<Integer>();
+//			keySet = tempNeighborNodeMap.keySet();
+//			ArrayList<Integer> arrListTempNodes = new ArrayList<Integer>();
+//
+//			for (Integer key : keySet) {
+//				arrListTempNodes = getNeighborNodesAscOrder(key);
+//
+//				for (int i = 0; i < arrListTempNodes.size(); i++) {
+//					if (arrListTempNodes.contains(arrListTempNodes.get(i))) {
+//						System.out.println(arrListTempNodes.get(i) + " in key set");
+//						continue;
+//					}
+//					System.out.println(arrListTempNodes.get(i));
+//				}
+//				//
+//				System.out.println("not head key: " + key);
+//
+//			}
+//
+//			for (Map.Entry<Integer, Double> entry : tempNeighborNodeMap.entrySet()) {
+//				System.out.println(
+//						"temp neighbor Key = " + entry.getKey() + ", temp neighbor Value = " + entry.getValue());
+//				finalReturnMap.put(entry.getKey(), entry.getValue());
+//			}
+//
+//			// System.out.println(intFoundNearestNeighbors);
+//			intFoundNearestNeighbors++;
+//
+//		}
+
+		// /// test start
+		// ArrayList<Integer> testResultList = new ArrayList<Integer>();
+		// testResultList = getNeighborNodes(3);
+		// System.out.println(testResultList);
+		//
+		// testResultList = getNeighborNodesAscOrder(3);
+		// System.out.println(testResultList);
+		//
+		// Map<Integer, Double> testResultSet = new HashMap<>();
+		// testResultSet = getNeighborNodesWithDistancesAscOrder(3);
+		// System.out.println(testResultSet);
+		// /// test end
+
+//		for (Map.Entry<Integer, Double> entry : finalReturnMap.entrySet()) {
+//			System.out.println("final return Key = " + entry.getKey() + ", final return Value = " + entry.getValue());
+//		}
+//
+//		for (int i = numberofK; i < numberofNeighbor; i++) {
+//			keyInt.remove(numberofK);
+//			valDoub.remove(numberofK);
+//		}
+//
+//		for (int i = 0; i < numberofK; i++) {
+//			pruned.put(keyInt.get(i), valDoub.get(i));
+//		}
+		System.out.println(numberOfK + " nearest neighbor nodes of " + sourceNode + ": " + mapFinalReturnNodes);
+
+		return mapFinalReturnNodes;
+
+	}
+
 	////////////////// ---Private methods----//////////////////
 	private Map<Integer, Double> getNeighborNodesWithDistances(int sourceNode) {
-		Map<Integer, Double> resultSet = new HashMap<>();
+		Map<Integer, Double> resultSet = new HashMap<Integer, Double>();
 		resultSet = graph.getAdjancencyMap().get(sourceNode);
 		return resultSet;
 	}
@@ -199,7 +370,7 @@ public class KnnAlgorithm implements Algorithm {
 	}
 
 	private Map<Integer, Double> getNeighborNodesWithDistancesAscOrder(int sourceNode) {
-		Map<Integer, Double> resultSet = new HashMap<>();
+		Map<Integer, Double> resultSet = new LinkedHashMap<Integer, Double>();
 		resultSet = getNeighborNodesWithDistances(sourceNode);
 
 		// Create a list from elements of HashMap
@@ -224,7 +395,7 @@ public class KnnAlgorithm implements Algorithm {
 
 	private ArrayList<Integer> getNeighborNodesAscOrder(int sourceNode) {
 
-		Map<Integer, Double> resultSet = new HashMap<>();
+		Map<Integer, Double> resultSet = new LinkedHashMap<Integer, Double>();
 		resultSet = getNeighborNodesWithDistances(sourceNode);
 
 		// Create a list from elements of HashMap
@@ -246,21 +417,24 @@ public class KnnAlgorithm implements Algorithm {
 
 		return arrListNeighborNodes;
 	}
-	
-	private boolean isNodeInList(ArrayList<Integer> list, int node) { 
-		
+
+	private boolean isNodeInList(ArrayList<Integer> list, int node) {
+
 		return list.contains(node);
 	}
-	private boolean isNodeInSet(Set<Integer> set, int node) { 
-		
+
+	private boolean isNodeInSet(Set<Integer> set, int node) {
+
 		return set.contains(node);
 	}
-	private boolean isNodeListInList(ArrayList<Integer> list, ArrayList<Integer> list2) { 
-		
+
+	private boolean isNodeListInList(ArrayList<Integer> list, ArrayList<Integer> list2) {
+
 		return list.containsAll(list2);
 	}
-	private boolean isNodeSetInSet(Set<Integer> set, Set<Integer> set2) { 
-		
+
+	private boolean isNodeSetInSet(Set<Integer> set, Set<Integer> set2) {
+
 		return set.containsAll(set2);
 	}
 
--- RTOPKImplementationInRoadNetwork/src/framework/Main.java
+++ RTOPKImplementationInRoadNetwork/src/framework/Main.java
@@ -14,20 +14,20 @@ import algorithm.KnnAlgorithm;
 public class Main {
 	public static void main(String[] args) {
 		UtilsManagment um = new UtilsManagment();
-
+		
 		// String edgeDatasetFile = "CAL-Edge_Eid-ESrc-EDest-EDist.csv";
 		String nodeDatasetFile = "Datasets/CAL-Node_NId-NLong-NLat.csv";
 		String poiDatasetFile = "Datasets/CAL-Poi_PoiLong-PoiLat-PoiCatId.csv";
 		String mergedPoiDatasetFile = "Datasets/CALMergerdPOI_Start-End-EdgeLen-NumofPOI.txt";
-		
+			
 		String mergedPoiDatasetFileShort = "Datasets/MergedPoi-short.txt";
 		String generatedDataset1 = "Datasets/GeneratedDataset2.txt";
 		String generatedDatasetForDijkstra1 = "Datasets/DatasetForDijkstra1.txt";
 		String manualEdgeDataset = "Datasets/ManualEdgeDataset1.txt";
 		String manualEdgeDataset2 = "Datasets/ManualEdgeDataset2.txt";
-
+		
 		//Graph roadNetwork2=new Graph();
-
+		
 		//System.out.println("Loading graph ...");
 		long startTimeGraphLoading = System.nanoTime();
 		// roadNetwork2 = um.readMergedPOI(mergedPoiDatasetFileShort);
@@ -35,42 +35,42 @@ public class Main {
 		// System.out.println();
 		// roadNetwork2.printPOIs();
 		long graphLoadingTime = System.nanoTime() - startTimeGraphLoading;
-
+		
 		// System.out.println("Loading nodes' information... ");
 		long startTimeLoadNodes = System.nanoTime();
 		//um.loadNodesInfo(roadNetwork2, nodeDatasetFile);
 		long nodesLoadingTime = System.nanoTime() - startTimeLoadNodes;
 		// roadNetwork2.printNodesInfo();
-
+		
 		//System.out.println("Loading pois' information... ");
 		long poisStartTime = System.nanoTime();
 		// um.loadPoiInfo(roadNetwork2, poiDatasetFile);
 		long poisLoadingTime = System.nanoTime() - poisStartTime;
 		// roadNetwork2.printPoisInfo();
-
+		
 		// long graphLoadingTimeSec = TimeUnit.SECONDS.convert(graphLoadingTime,
 		// TimeUnit.NANOSECONDS);
 		// long nodesLoadingTimeSec = TimeUnit.SECONDS.convert(nodesLoadingTime,
 		// TimeUnit.NANOSECONDS);
 		// long poisLoadingTimeSec = TimeUnit.SECONDS.convert(poisLoadingTime,
 		// TimeUnit.NANOSECONDS);
-
+		
 		double graphLoadingTimeD = (double) graphLoadingTime / 1000000000.0;
 		double nodesLoadingTimeD = (double) nodesLoadingTime / 1000000000.0;
 		double poisLoadingTimeD = (double) poisLoadingTime / 1000000000.0;
-
+		
 		// System.out.println("Elapsed time of Graph loading: " + graphLoadingTimeD + "
 		// seconds");
 		// System.out.println("Elapsed time of Nodes info loading: " + nodesLoadingTimeD
 		// + " seconds");
 		// System.out.println("Elapsed time of POIs info loading: " + poisLoadingTimeD +
 		// " seconds");
-
+		
 		// roadNetwork2.printPoisInfo();
 		// roadNetwork2.printEdgesInfo();
 		// Graph roadNetwork3 = um.readMergedPOI(generatedDataset1);
 		// roadNetwork3.printGraph();
-
+		
 		// =============Dijkstra TEST=================================
 		// Scanner sc = new Scanner(System.in);
 		// System.out.println("-----Test of Dijkstra Algorith-----");
@@ -89,16 +89,16 @@ public class Main {
 		// dijkstra.execute(roadNetwork4.getNodesWithInfo().get(sourceId));
 		// LinkedList<Node> path =
 		// dijkstra.getPath(roadNetwork4.getNodesWithInfo().get(destinationId));
-
+		
 		// assertNotNull(path);
 		// assertTrue(path.size() > 0);
-
+		
 		// for (Node vertex : path) {
 		// System.out.println(vertex);
 		// }
-
+		
 		// =============End of Dijkstra TEST=================================
-
+		
 		// =============OLD TESTS=================================
 		// roadNetwork1.addEdge(0, 1, 2.1);
 		// roadNetwork1.addEdge(0, 4, 3.2);
@@ -116,12 +116,12 @@ public class Main {
 		// roadNetwork1.printGraph();
 		// roadNetwork1.removeEdge(2, 1);
 		// roadNetwork1.removeEdge(3, 4);
-
+		
 		// //roadNetwork1.printGraph();
 		// System.out.println(roadNetwork1.getEdges(1));
 		// System.out.println(roadNetwork1.getEdgesWithDistances(1));
 		// roadNetwork1.getEdges(1);
-
+		
 		// roadNetwork1.addPOI(31, 0, 1, 1.3);
 		// roadNetwork1.addPOI(32, 0, 1, 1.9);
 		// roadNetwork1.addPOI(33, 0, 4, 2.2);
@@ -130,7 +130,7 @@ public class Main {
 		//
 		// System.out.println(roadNetwork1.getPOIs(0, 1));
 		// System.out.println(roadNetwork1.getPOIsWithDistance(0, 1));
-
+		
 		// ArrayList<Vertex> myVertices =
 		// um.readVertexFiles("CAL-Vertex_Vid-VLong-VLat.csv");
 		// System.out.println("Vertex File Imported Successfully!");
@@ -148,13 +148,13 @@ public class Main {
 		//
 		// um.populatePOIMap("CAL-POI_POIId-POIName-POIType.csv");
 		// um.displayPOIHmap();
-
+		
 		
 		//--------Generator Algorithms TESTS-----------------------
 		//Graph roadNetwork5 = um.readMergedPOI(generatedDatasetForDijkstra1);
 //		UserPreferenceGenerator upg = new UserPreferenceGenerator();
 //		upg.generateRandomUserPreference(600, 2);
-
+		
 		//Graph newGraph2 = new Graph();
 		//PoiGeneratorAlgorithm pga = new PoiGeneratorAlgorithm();
 		//pga.generateRandomPois(roadNetwork5, 0, 0, 0, 0);
@@ -178,14 +178,14 @@ public class Main {
 		roadNetwork7.printGraph();
 		
 		KnnAlgorithm knnAlg2 = new KnnAlgorithm(roadNetwork7);
-		knnAlg2.getKNNNodes(4, 3);
-		knnAlg2.getKNNNodesWithDistance(4, 3);	
+//		knnAlg2.getKNNNodes(4, 3);
+//		knnAlg2.getKNNNodesWithDistance(4, 3);	
 		
 		System.out.println("-------------------");
-		knnAlg2.getKNNNodesWithDistance2(4, 4);
-
+		knnAlg2.getKNNNodesWithDistance3(4, 4);
 		
-
+		
+		
 	}
 
 }
