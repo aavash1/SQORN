bfa04969f437a39b60b78ac65d2d447da4b7e50b
Aavash <aavashbhandari@gmail.com>
2020-11-20 21:17:02 +0900

    Test commit

---
+++ src/visualization/PointDraw.java
@@ -0,0 +1,136 @@
+package visualization;
+
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+import framework.Graph;
+import framework.Graph;
+import framework.Node;
+import framework.UtilsManagment;
+
+public class PointDraw extends JFrame {
+
+	// private final static Color COLOR_EDGE_LINE = Color.black;
+	private final static Color COLOR_NODE_CIRCLE = Color.blue;
+	private final static Color COLOR_NODE_ID = Color.red;
+
+	private int nodeCircleWidth;
+	private int nodeCircleHeight;
+	
+	private static String str = "5";
+
+	// Graph graph;
+
+	ArrayList<Vector2D> points;
+
+	// if Node/Edge/Poi added to existing and visualized graph, then need to repaint
+	// using local method: this.repaint();
+	public PointDraw() { // Constructor
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		nodeCircleWidth = 5;
+		nodeCircleHeight = 5;
+	}
+
+	public PointDraw(String name) { // Construct with label
+		this.setTitle(name);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		nodeCircleWidth = 5;
+		nodeCircleHeight = 5;
+	}
+
+	// public void setGraph(Graph g) {
+	// this.graph = g;
+	// }
+
+	public void setPoints(ArrayList<Vector2D> points) {
+		this.points = points;
+	}
+
+	public void paint(Graphics g) { // draw the nodes and edges
+		FontMetrics f = g.getFontMetrics();
+		int nodeHeight = Math.max(nodeCircleHeight, f.getHeight());
+		for (Vector2D point : points) {
+			
+			int nodeWidth = Math.max(nodeCircleWidth, f.stringWidth(str) + nodeCircleWidth / 2);
+			g.setColor(COLOR_NODE_CIRCLE);
+			g.fillOval((int) (point.getX() - nodeWidth / 2), (int) (point.getY() - nodeHeight / 2), nodeWidth,
+					nodeHeight);
+			g.setColor(COLOR_NODE_ID);
+			g.drawOval((int) (point.getX() - nodeWidth / 2), (int) (point.getY() - nodeHeight / 2), nodeWidth,
+					nodeHeight);
+			int x, y;
+			x = UtilsManagment.convertDoubleToInteger(point.getX() - f.stringWidth("+") / 2);
+			y = UtilsManagment.convertDoubleToInteger(point.getY() + f.getHeight() / 2);
+			g.drawString("+", x, y);
+		}
+
+	}
+
+	// These 5 methods might be possible in future
+	private double calculateDistance(Node n1, Node n2) {
+		return Point2D.distance(n1.getLatitude(), n1.getLongitude(), n2.getLatitude(), n2.getLongitude());
+	}
+
+	private boolean isTriangle(int a, int b, int c) {
+
+		if ((a + b < c) || (a + c < b) || (b + c < a))
+			return false;
+
+		return true;
+	}
+
+	private boolean isTriangle(double a, double b, double c) {
+
+		if ((a + b < c) || (a + c < b) || (b + c < a))
+			return false;
+
+		return true;
+	}
+
+	private boolean isTriangle(Node n1, Node n2, Node n3) {
+
+		if ((calculateDistance(n1, n2) + calculateDistance(n1, n3) < calculateDistance(n2, n3))
+				|| (calculateDistance(n1, n3) + calculateDistance(n2, n3) < calculateDistance(n1, n2))
+				|| (calculateDistance(n1, n2) + calculateDistance(n2, n3) < calculateDistance(n1, n3)))
+			return false;
+
+		return true;
+	}
+
+	private Node findPossibleThirdNodeCoordinates(Node n1, Node n2) {
+
+		Node n3 = null;
+		for (int i = 0; i < Math.max(nodeCircleWidth, nodeCircleHeight); i++) {
+			for (int j = 0; j < Math.max(nodeCircleWidth, nodeCircleHeight); j++) {
+				n3.setLatitude((double) i);
+				n3.setLongitude((double) j);
+				if (isTriangle(n1, n2, n3)) {
+					return n3;
+				}
+			}
+		}
+		return n3;
+	}
+	private Node findPossibleFourthNodeCoordinates(Node n1, Node n2) {
+
+		Node n3 = null;
+		for (int i = 0; i < Math.max(nodeCircleWidth, nodeCircleHeight); i++) {
+			for (int j = 0; j < Math.max(nodeCircleWidth, nodeCircleHeight); j++) {
+				n3.setLatitude((double) i);
+				n3.setLongitude((double) j);
+				if (isTriangle(n1, n2, n3)) {
+					return n3;
+				}
+			}
+		}
+		return n3;
+	}
+
+}
